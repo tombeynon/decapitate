9634d87882b174c4ddfe79b4b0392f7b15868733
Tom Beynon <tbeynon@rawnet.com>
2013-04-12 09:04:13 +0100

    Added meta description and event tracking

---
+++ javascripts/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("table.decapitate").decapitate();
+	
+	$('a.download').on('click', function(event) {
+		target = $(this).attr("rel");
+		url = $(this).attr("href");
+		if(target != "" && target != undefined) ga('send', 'event', 'download', target, url);
+	});
+	
+	$('a.view').on('click', function(event) {
+		target = $(this).attr("rel");
+		url = $(this).attr("href");
+		if(target != "" && target != undefined) ga('send', 'event', 'view', target, url);
+	});
+	
+});
